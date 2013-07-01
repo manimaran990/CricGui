81163e3c5faa66ea7ffe5f4539b1b77bc5f684e6
manimaran <manimaran990@gmail.com>
2013-07-01 22:33:03 +0530

    more refinements added reload and exit button and those facilities

--- src/CricGui.java
+++ src/CricGui.java
@@ -3,20 +3,52 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class CricGui extends JFrame implements ActionListener {
+    
+    public JTextArea scoreBoard = new JTextArea(15,19);
     public CricGui() {
         super("CricInfo");
-        setSize(300,300);
+        setSize(350,350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLookAndFeel();
-        CricPanel cp=new CricPanel();
-        FlowLayout fl=new FlowLayout();
-        setLayout(fl);
-        JLabel noti=new JLabel("CURRENT MATCHES");
+        BorderLayout bl = new BorderLayout();
+        setLayout(bl);
+        
+        //first panel
+        JPanel firstPanel=new JPanel();
+        FlowLayout fl = new FlowLayout(FlowLayout.CENTER,5,5);
+        firstPanel.setLayout(fl);
+        JLabel noti = new JLabel("SCORE BOARD");
+        firstPanel.add(noti);
+        add(firstPanel,BorderLayout.NORTH);
+        
+        
+        
+        //second panel
+        JPanel secPanel = new JPanel();
+        FlowLayout fl1 = new FlowLayout(FlowLayout.CENTER,21,21);
+        CricPanel cp = new CricPanel();
+        scoreBoard.setText(cp.getScores());
+        scoreBoard.setEditable(false);
+        secPanel.add(scoreBoard);
+        secPanel.setLayout(fl1);
+        add(secPanel,BorderLayout.CENTER);
+        
+        //third panel
+        JPanel thirdPanel = new JPanel();
+        FlowLayout fl2 = new FlowLayout(FlowLayout.CENTER,5,5);
+        thirdPanel.setLayout(fl2);
         JButton Reload=new JButton("Reload");
+        JButton exit = new JButton(" Exit ");
         Reload.addActionListener(this);
-        add(noti);
-        add(cp);
-        add(Reload);
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+           }
+         });
+        thirdPanel.add(Reload);
+        thirdPanel.add(exit);
+        add(thirdPanel,BorderLayout.SOUTH);
+
                
         setVisible(true);
     }
@@ -35,6 +67,12 @@ public class CricGui extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         //reload scores
+        
+        CricPanel cp = new CricPanel();
+        scoreBoard.setText(cp.getScores());
+        scoreBoard.repaint();
+        
+        
     }
 
  }
